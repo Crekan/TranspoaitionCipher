a062c5096404f7be27304e85c8f6a9cbe48e30f9
Crekan <Crekan@github.com>
2020-05-29 22:36:30 +0300

    TranspoaitionCipher
    TranspoaitionCipher

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace TranspoaitionCipher
+{
+    partial class Шифр
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.KeyTextBox = new System.Windows.Forms.TextBox();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.EncryptRadioButton = new System.Windows.Forms.RadioButton();
+            this.decryptRadioButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(131, 43);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(296, 20);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // KeyTextBox
+            // 
+            this.KeyTextBox.Location = new System.Drawing.Point(228, 172);
+            this.KeyTextBox.Name = "KeyTextBox";
+            this.KeyTextBox.Size = new System.Drawing.Size(199, 20);
+            this.KeyTextBox.TabIndex = 1;
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(131, 6);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(296, 20);
+            this.InputTextBox.TabIndex = 2;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(258, 101);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(136, 50);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Выполнить";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.decryptRadioButton);
+            this.groupBox1.Controls.Add(this.EncryptRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(15, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(130, 85);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Опция";
+            // 
+            // EncryptRadioButton
+            // 
+            this.EncryptRadioButton.AutoSize = true;
+            this.EncryptRadioButton.Checked = true;
+            this.EncryptRadioButton.Location = new System.Drawing.Point(7, 20);
+            this.EncryptRadioButton.Name = "EncryptRadioButton";
+            this.EncryptRadioButton.Size = new System.Drawing.Size(95, 17);
+            this.EncryptRadioButton.TabIndex = 0;
+            this.EncryptRadioButton.TabStop = true;
+            this.EncryptRadioButton.Text = "Зашифровать";
+            this.EncryptRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // decryptRadioButton
+            // 
+            this.decryptRadioButton.AutoSize = true;
+            this.decryptRadioButton.Location = new System.Drawing.Point(7, 43);
+            this.decryptRadioButton.Name = "decryptRadioButton";
+            this.decryptRadioButton.Size = new System.Drawing.Size(107, 17);
+            this.decryptRadioButton.TabIndex = 1;
+            this.decryptRadioButton.Text = "Расшифрорвать";
+            this.decryptRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Исходный текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Обработанный текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ключ";
+            // 
+            // Шифр
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 221);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.KeyTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "Шифр";
+            this.Text = "Шифр перестановки";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.TextBox KeyTextBox;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton decryptRadioButton;
+        private System.Windows.Forms.RadioButton EncryptRadioButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TranspoaitionCipher
+{
+    public partial class Шифр : Form
+    {
+        Transposition t;
+        public Шифр()
+        {
+            InitializeComponent();
+
+            t = new Transposition();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            t.SetKey(KeyTextBox.Text);
+
+            if (EncryptRadioButton.Checked)
+                outputTextBox.Text = t.Encrypt(InputTextBox.Text);
+            else
+                outputTextBox.Text = t.Decrypt(InputTextBox.Text);
+        }
+    }
+}
